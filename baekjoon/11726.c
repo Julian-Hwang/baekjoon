34b4bf8e4f5cd77db47148c902c58eac8e77cad7
석진 황 <julian5383@naver.com>
2022-08-10 21:18:58 +0900

    13번째 커밋

---
+++ baekjoon/11726.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    int D[1010] = { 0, };
+
+    D[0] = 1;
+    D[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        D[i] = (D[i - 1] + D[i - 2]) % 10007;
+    }
+    printf("%d\n", D[n] % 10007);
+    return 0;
+}
